d492a7975eaade5b52eb5b5f0e732263d12dada8
Jazmin18 <39746253+Jazmin18@users.noreply.github.com>
2020-02-23 17:11:45 -0500

    Mensajeria-inst-backgroudwhite

--- frontend/src/app/services/verchats.service.ts
+++ frontend/src/app/services/verchats.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -6,46 +8,9 @@ import { Injectable } from '@angular/core';
 export class VerchatsService {
 
   URL: string = 'http://localhost:3000';
-  authSubject = new BehaviorSubject(false);
   private token: string;
 
   constructor(
     private http: HttpClient, private router: Router
   ) { }
-
-  signUp(user) {
-    return this.http.post<any>(`${this.URL}/register`, user);
-  }
-
-  login(user) {
-    return this.http.post<any>(this.URL + '/login', user);
-  }
-
-  getUsers() {
-    return this.http.get<any>(this.URL);
-  }
-
-  getUserLogin(id) {
-    return this.http.post<any>(this.URL, id);
-  }
-
-  loggedIn() {
-    return !!localStorage.getItem('token');
-  }
-
-  getToken() {
-    let token = localStorage.getItem('token');
-    return token;
-  }
-
-  getId() {
-    let id = localStorage.getItem('id');
-    return id;
-  }
-
-  logout() {
-    localStorage.removeItem('id');
-    localStorage.removeItem('token');
-    this.router.navigate(['/']);
-  }
 }
