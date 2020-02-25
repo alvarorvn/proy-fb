52c139bbb95c40240f4f6afdae724261ba674693
Alvaro <alvaro.rvn97@gmail.com>
2020-02-25 13:31:38 -0500

    Acceso visual al perfil de otros usuarios

--- frontend/src/app/components/perfil-amigo/perfil-amigo.component.ts
+++ frontend/src/app/components/perfil-amigo/perfil-amigo.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { AuthService } from "../../services/auth.service";
 import { DomSanitizer } from '@angular/platform-browser';
+
+import { AuthService } from "../../services/auth.service";
+import { PerfilAmigoService } from "../../services/perfil-amigo.service";
 
 @Component({
   selector: 'app-perfil-amigo',
@@ -13,19 +15,40 @@ export class PerfilAmigoComponent implements OnInit {
   amigo_id = "";
   perfil_amigo = {};
 
+  // Listas de cosas
+  seguidores: Array<Object> = [];
+  seguidos: Array<Object> = [];
+  amigos: Array<Object> = [];
+  empleos: Array<Object> = [];
+  aptitudes: Array<Object> = [];
+  estudios: Array<Object> = [];
+  telefonos: Array<Object> = [];
+  direcciones: Array<Object> = [];
+  apodos: Array<Object> = [];
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private authService: AuthService,    
-    private sanitizer: DomSanitizer
+    private authService: AuthService,
+    private sanitizer: DomSanitizer,
+    private perfAmigo: PerfilAmigoService
   ) {
     this.amigo_id = this.route.snapshot.params.idamigo;
   }
 
-  ngOnInit() {    
+  ngOnInit() {
     this.getPerfilAmigo();
+    this.getAmigos();
+    this.getSeguidores();
+    this.getSeguidos();
     setTimeout(() => {
-    }, 800);
+      this.getEmpleos();
+      this.getAptitudes();
+      this.getEstudios();
+      this.getTelefonos();
+      this.getApodos();
+      this.getDirecciones();
+    }, 1000);
   }
 
   getPerfilAmigo() {
@@ -36,5 +59,95 @@ export class PerfilAmigoComponent implements OnInit {
 
   public getSantizeUrl(url: string) {
     return this.sanitizer.bypassSecurityTrustResourceUrl(`data:image/png;charset=utf-8;base64, ${url}`);
+  }
+
+  getAmigos() {
+    this.perfAmigo.getAmigos(this.amigo_id).subscribe(res => {
+      if (res.tipo == 'error') {
+        this.amigos = res.result;
+      } else {
+        this.amigos = res;
+      }
+    })
+  }
+
+  getSeguidores() {
+    this.perfAmigo.getSeguidores(this.amigo_id).subscribe(res => {
+      if (res.tipo == 'error') {
+        this.seguidores = res.result;
+      } else {
+        this.seguidores = res;
+      }
+    })
+  }
+
+  getSeguidos() {
+    this.perfAmigo.getSeguidos(this.amigo_id).subscribe(res => {
+      if (res.tipo == 'error') {
+        this.seguidos = res.result;
+      } else {
+        this.seguidos = res;
+      }
+    })
+  }
+
+  getEmpleos() {
+    this.perfAmigo.getEmpleos(this.perfil_amigo['perfilusu_id']).subscribe(res => {
+      if (res.tipo == 'error') {
+        this.empleos = res.result;
+      } else {
+        this.empleos = res;
+      }
+    })
+  }
+
+  getAptitudes() {
+    this.perfAmigo.getAptitud(this.perfil_amigo['perfilusu_id']).subscribe(res => {
+      if (res.tipo == 'error') {
+        this.aptitudes = res.result;
+      } else {
+        this.aptitudes = res;
+      }
+    })
+  }
+
+  getEstudios() {
+    this.perfAmigo.getEstudio(this.perfil_amigo['perfilusu_id']).subscribe(res => {
+      if (res.tipo == 'error') {
+        this.estudios = res.result;
+      } else {
+        this.estudios = res;
+      }
+    })
+  }
+
+  getTelefonos() {
+    this.perfAmigo.getTelf(this.perfil_amigo['perfilusu_id']).subscribe(res => {
+      if (res.tipo == 'error') {
+        this.telefonos = res.result;
+      } else {
+        this.telefonos = res;
+      }
+    })
+  }
+
+  getDirecciones() {
+    this.perfAmigo.getDireccion(this.perfil_amigo['perfilusu_id']).subscribe(res => {
+      if (res.tipo == 'error') {
+        this.direcciones = res.result;
+      } else {
+        this.direcciones = res;
+      }
+    })
+  }
+
+  getApodos() {
+    this.perfAmigo.getApodo(this.perfil_amigo['perfilusu_id']).subscribe(res => {
+      if (res.tipo == 'error') {
+        this.apodos = res.result;
+      } else {
+        this.apodos = res;
+      }
+    })
   }
 }
--- frontend/src/app/services/perfil-amigo.service.ts
+++ frontend/src/app/services/perfil-amigo.service.ts
@@ -1,9 +1,169 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from "./auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PerfilAmigoService {
 
-  constructor() { }
+  URL: string = 'http://localhost:3000';
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) { }
+
+  getSeguidores(id_amigo) {
+    return this.http.get<any>(`${this.URL}/${id_amigo}/seguidores`);
+  }
+
+  getAmigos(id_amigo) {
+    return this.http.get<any>(`${this.URL}/${id_amigo}/amigos`);
+  }
+
+  getSeguidos(id_amigo) {
+    return this.http.get<any>(`${this.URL}/${id_amigo}/seguidos`);
+  }
+
+  updatePerfilPhoto(photo) {
+    return this.http.post<any>(`${this.URL}/${this.authService.getId()}/update-perfil-photo`, photo);
+  }
+
+  updatePortadaPhoto(photo) {
+    return this.http.post<any>(`${this.URL}/${this.authService.getId()}/update-portada-photo`, photo);
+  }
+
+  // EMPLEOS
+  addEmpleo(empleo) {
+    return this.http.post<any>(`${this.URL}/${this.authService.getId()}/biografia/add-empleo`, empleo);
+  }
+
+  getEmpleos(perfil_id) {
+    return this.http.get<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/get-empleo`);
+  }
+
+  updateEmpleos(perfil_id, empleo) {
+    return this.http.put<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/update-empleo/${empleo['empleo_id']}`, empleo);
+  }
+
+  deleteEmpleos(perfil_id, empleo_id) {
+    return this.http.delete<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/delete-empleo/${empleo_id}`);
+  }
+  // FIN EMPLEOS
+
+  // APTITUDES
+  addAptitud(aptitud) {
+    return this.http.post<any>(`${this.URL}/${this.authService.getId()}/biografia/add-aptitud`, aptitud);
+  }
+
+  getAptitud(perfil_id) {
+    return this.http.get<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/get-aptitudes`);
+  }
+
+  updateAptitud(perfil_id, aptitud) {
+    return this.http.put<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/update-aptitud/${aptitud['habprof_id']}`, aptitud);
+  }
+
+  deleteAptitud(perfil_id, aptitud_id) {
+    return this.http.delete<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/delete-aptitud/${aptitud_id}`);
+  }
+  // FIN APTITUDES
+
+  // ESTUDIOS
+  addEstudio(estudio) {
+    return this.http.post<any>(`${this.URL}/${this.authService.getId()}/biografia/add-estudio`, estudio);
+  }
+
+  getEstudio(perfil_id) {
+    return this.http.get<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/get-estudios`);
+  }
+
+  updateEstudio(perfil_id, estudio) {
+    return this.http.put<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/update-estudio/${estudio['univ_id']}`, estudio);
+  }
+
+  deleteEstudio(perfil_id, univ_id) {
+    return this.http.delete<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/delete-estudio/${univ_id}`);
+  }
+  // FIN ESTUDIOS
+
+
+  // TELEFONOS
+  addTelf(telf) {
+    return this.http.post<any>(`${this.URL}/${this.authService.getId()}/biografia/add-telf`, telf);
+  }
+
+  getTelf(perfil_id) {
+    return this.http.get<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/get-telf`);
+  }
+
+  updateTelf(perfil_id, telf) {
+    return this.http.put<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/update-telf/${telf['telf_id']}`, telf);
+  }
+
+  deleteTelf(perfil_id, telf_id) {
+    return this.http.delete<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/delete-telf/${telf_id}`);
+  }
+  // FIN TELEFONOS
+
+  // DIRECCION
+  addDireccion(direccion) {
+    return this.http.post<any>(`${this.URL}/${this.authService.getId()}/biografia/add-direccion`, direccion);
+  }
+
+  getDireccion(perfil_id) {
+    return this.http.get<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/get-direccion`);
+  }
+
+  updateDireccion(perfil_id, direccion) {
+    return this.http.put<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/update-direccion/${direccion['dir_id']}`, direccion);
+  }
+
+  deleteDireccion(perfil_id, dir_id) {
+    return this.http.delete<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/delete-direccion/${dir_id}`);
+  }
+  // FIN DIRECCION
+
+  // RELIGION
+  updateReligion(perfil_id, religion) {
+    return this.http.put<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/update-religion`, religion);
+  }
+  // FIN RELIGION
+
+  // INTERES
+  updateInteres(perfil_id, interes) {
+    return this.http.put<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/update-interes`, interes);
+  }
+  // FIN INTERES
+
+  // INFORMACION
+  updateInformacion(perfil_id, info) {
+    return this.http.put<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/update-info`, info);
+  }
+  // FIN INTERES
+
+  // USERLOGIN
+  updateUserLogin(userLogin) {
+    return this.http.put<any>(`${this.URL}/${this.authService.getId()}`, userLogin);
+  }
+  // FIN USERLOGIN
+
+  // APODOS
+  addApodo(apodo) {
+    return this.http.post<any>(`${this.URL}/${this.authService.getId()}/biografia/add-apodo`, apodo);
+  }
+
+  getApodo(perfil_id) {
+    return this.http.get<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/get-apodo`);
+  }
+
+  updateApodo(perfil_id, apodo) {
+    return this.http.put<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/update-apodo/${apodo['apodo_id']}`, apodo);
+  }
+
+  deleteApodo(perfil_id, apodo_id) {
+    return this.http.delete<any>(`${this.URL}/${this.authService.getId()}/biografia/${perfil_id}/delete-apodo/${apodo_id}`);
+  }
+  // FIN APODOS
 }
