c83af39b76ac4a4df8272deaffbc5572df54385e
Alvaro <alvaro.rvn97@gmail.com>
2020-02-25 11:14:52 -0500

    Enlace a otras biografias

--- frontend/src/app/components/perfil-amigo/perfil-amigo.component.ts
+++ frontend/src/app/components/perfil-amigo/perfil-amigo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from "../../services/auth.service";
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-perfil-amigo',
@@ -9,16 +11,30 @@ import { Router, ActivatedRoute } from '@angular/router';
 export class PerfilAmigoComponent implements OnInit {
 
   amigo_id = "";
+  perfil_amigo = {};
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute
-  ) { 
+    private route: ActivatedRoute,
+    private authService: AuthService,    
+    private sanitizer: DomSanitizer
+  ) {
     this.amigo_id = this.route.snapshot.params.idamigo;
   }
 
-  ngOnInit() {
-    console.log(this.amigo_id);
+  ngOnInit() {    
+    this.getPerfilAmigo();
+    setTimeout(() => {
+    }, 800);
   }
 
+  getPerfilAmigo() {
+    this.authService.getUserLogin({ usuario_id: this.amigo_id }).subscribe(res => {
+      this.perfil_amigo = res;
+    })
+  }
+
+  public getSantizeUrl(url: string) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(`data:image/png;charset=utf-8;base64, ${url}`);
+  }
 }
