dde5dffadd53587410352b198a463ceca2a1c674
JuanCuestaV <juanbryanc@gmail.com>
2020-02-25 21:05:40 -0500

    Publicaciones SELECT, DELETE, INSERT

--- frontend/src/app/Post.ts
+++ frontend/src/app/Post.ts
@@ -2,4 +2,5 @@ export class Post {
     pub_id: number;
     pub_texto: string;
     usuario_id: number;
+    n?: number;
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,13 +1,15 @@
 const { Router } = require('express');
 const validate = require('../src/validaciones.js');
-const { savepost, getPostsUser, deletePost } = require('../controllers/posts.js');
+const { savepost, getPostsUser, deletePost, updatePost } = require('../controllers/posts.js');
 
 // objeto que permite definir rutas
 const router = Router();
 
-router.post('/savepost', savepost);             // Ruta de registro de usuario
-router.get('/getpostsuser/:usuario_id', getPostsUser);     // Obtiene todas las publicaciones del usuario especificado
+router.post('/savepost', savepost);                                                     // Ruta de registro de usuario
+router.get('/getpostsuser/:usuario_id', getPostsUser);                                  // Obtiene todas las publicaciones del usuario especificado
 router.delete('/deletepost/:pub_id', deletePost);
+router.put('/updatepost/:pub_id', updatePost);
+//router.post('/:id/addphoto', validate.verifyToken, user.updatePerfilPhoto);  // Agregar foto a post
 
 //exportar objeto
 module.exports = router;
